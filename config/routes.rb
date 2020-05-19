7d2270449c1134e355c32efe86f92f5cd8b84ba1
Andres Camacho <andres@bettertherapeutics.io>
2020-05-19 10:08:14 -0700

    Fix for rails 5 #172823818

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  not_found_response = ->(env) { [404, {}, []] }
+
   BatchRequestApi.config.batch_sequential_paths.each do |batch_sequential_path|
-    post batch_sequential_path, constraints: { format: :json }
+    post batch_sequential_path, constraints: { format: :json }, to: not_found_response
   end
 
   BatchRequestApi.config.batch_parallel_paths.each do |batch_parallel_path|
-    post batch_parallel_path, constraints: { format: :json }
+    post batch_parallel_path, constraints: { format: :json }, to: not_found_response
   end
 end
